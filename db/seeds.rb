c983b0155c5462d964bd0a876ba835a573990b52
Robin Handous <robin.handous@gmail.com>
2017-11-02 16:27:06 -0400

    some views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
 require 'json'
 
-Ingredients.destroy_all
+Ingredient.destroy_all
 
 url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 json_file_serialized = open(url).read
 json_file = JSON.parse(json_file_serialized)
-json_file[“drinks”].each do |d|
+json_file["drinks"].each do |d|
   Ingredient.create(name: d["strIngredient1"])
 end
